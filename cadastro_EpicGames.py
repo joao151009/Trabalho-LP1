8e30140b4ec01d05cfc86545af20a055a7260393
joao151009 <150694546+joao151009@users.noreply.github.com>
2025-02-19 09:39:51 -0300

    Add files via upload

--- cadastro_EpicGames.py
+++ cadastro_EpicGames.py
@@ -1,10 +1,10 @@
 import json
 print('Bem vindo ao cadastro da epic games!!')
 def ler_valor_nao_vazio(nome_variavel):
-    valor_lido = input(f'Digite o/a {nome_variavel}: ')
+    valor_lido = input(f'Digite o {nome_variavel}: ')
     while valor_lido == '': 
         print(f'{nome_variavel} não pode ficar vazio!')
-        valor_lido = input(f'Digite o/a {nome_variavel}:')
+        valor_lido = input(f'Digite o {nome_variavel}:')
     return valor_lido
 
 def confirmaSenha(senha):
@@ -70,33 +70,63 @@ def mostrarCadastros():
                     print(f"senha:\t\t{item['senha']}")
                     print('\n')
 
+def removerCadastros():
+        removedor = int(input("Qual cadastro você deseja apagar? (Ex.: 1, 2, 3...): "))
+        removedor -= 1
+        registro.pop(removedor)
+        print('Cadastro removido com sucesso!!!!')
+
 def editarCadastros():
-     print()
+        editor = int(input("Qual cadastro você deseja editar? (Ex.: 1, 2, 3...): "))
+        editor -= 1
+        modificarDados = int(input("Qual dado você quer editar?\n 1-Nome \n 2-Plataforma \n 3-Email \n 4-Senha: \n"))
+        if modificarDados == 1:
+            novo_nome = ler_valor_nao_vazio('nome')
+            registro[editor]['nome'] = novo_nome
+            print('Cadastro alterado com sucesso!')
+            salvarJson(registro)
+        elif modificarDados == 2:
+            nova_plataforma = lerPlataforma('plataforma')
+            registro[editor]['plataforma'] = nova_plataforma
+            print('Cadastro alterado com sucesso!')
+            salvarJson(registro)
+        elif modificarDados == 3:
+            novo_email = ler_valor_nao_vazio('email')
+            registro[editor]['email'] = novo_email
+            print('Cadastro alterado com sucesso!')
+            salvarJson(registro)
+        elif modificarDados == 4:
+            nova_senha = confirmaSenha('senha')
+            registro[editor]['senha'] = nova_senha
+            print('Cadastro alterado com sucesso!')
+            salvarJson(registro)
+        print('Digite um valor válido!')
 registro = lerJson()
 
 while True:
-        
-        menu = int(input("Digite:\n 1 para mostrar os cadastros, 2 para criar um novo cadastro, 3 para excluir um cadastro, 4 para editar algum cadastro e 0 para sair: "))
-        if menu == 1:
-            mostrarCadastros()
-        elif menu == 2:
-            item = cadastroEpicGames()
-            registro.append(item)
-            salvarJson(registro)
-            print("Cadastro feito com sucesso!!! YAY!!!")
-        elif menu == 3:
-            try:
+        try:
+            menu = int(input("Digite:\n 1 para mostrar os cadastros, 2 para criar um novo cadastro, 3 para excluir um cadastro, 4 para editar algum cadastro e 0 para sair: "))
+            if menu == 1:
                 mostrarCadastros()
-                removedor = int(input("Qual cadastro você deseja apagar? (Ex.: 1, 2, 3...): "))
-                removedor -= 1
-                registro.pop(removedor)
-                print('Cadastro removido com sucesso!!!!')
-            except:
-                print("Esse cadastro não existe!")
-        elif menu == 4:
-            mostrarCadastros()
-            editor = int(input("Qual cadastro você deseja editar? (Ex.: 1, 2, 3...): "))
-            editor -= 1
-            
-        elif menu == 0:
-            break
+            elif menu == 2:
+                item = cadastroEpicGames()
+                registro.append(item)
+                print("Cadastro feito com sucesso!!! YAY!!!")
+            elif menu == 3:
+                mostrarCadastros()
+                while True:
+                    try:
+                        removerCadastros()
+                    except:
+                        print('Esse cadastro não existe!!')
+            elif menu == 4:
+                mostrarCadastros()
+                while True:
+                    try:
+                        editarCadastros()
+                    except:
+                        print('Insira um valor válido!')
+            elif menu == 0:
+                break
+        except:
+            print('Digite um valor válido!')
